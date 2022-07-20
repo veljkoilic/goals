934f8e539f19d478e79baceeb9ed597f54f26280
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-20 19:54:32 +0200

    refactored with routes file

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,17 +1,14 @@
 const express = require('express')
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.status(200).json({"message":'Eo goals bro'})
-})
-router.post('/', (req, res) => {
-    res.status(200).json({"message":'Set Goal'})
-})
-router.put('/:id', (req, res) => {
-    res.status(200).json({"message":`Update goal ${req.params.id}`})
-})
-router.delete('/:id', (req, res) => {
-    res.status(200).json({"message":`Delete goal ${req.params.id}`})
-})
+router.get('/', getGoals)
+router.post('/', setGoal)
+router.put('/:id', updateGoal)
+router.delete('/:id', deleteGoal)
+
+//Less lines but also less readable (For me)
+// router.route('/').get(getGoals).post(setGoal)
+// router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
